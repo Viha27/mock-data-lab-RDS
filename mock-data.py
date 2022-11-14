8423551e3796a7e4ea45a1064f63223275a2a1d2
David Blocher <david-blocher@pluralsight.com>
2022-11-14 15:59:47 -0700

    first commit

---
+++ mock-data.py
@@ -0,0 +1,28 @@
+import pandas
+from faker import Faker
+from collections import defaultdict
+from sqlalchemy import create_engine
+import boto3
+
+ENDPOINT=""
+PORT="3306"
+USER="adm
+PASS="password"
+DBNAME="mockdatabase"
+
+fake = Faker()
+
+mock_data = defaultdict(list)
+
+for _ in range(2000):
+    mock_data["first_name"].append( fake.first_name() )
+    mock_data["last_name"].append( fake.last_name() )
+    mock_data["job"].append( fake.job() )
+    mock_data["dob"].append( fake.date_of_birth() )
+    mock_data["country"].append( fake.country() )
+    
+df_mock_data = pandas.DataFrame(mock_data)
+
+engine = create_engine(f'mysql+pymysql://{USER}:{PASS}@{ENDPOINT}:{PORT}/{DBNAME}', echo=False)
+
+df_mock_data.to_sql('use', con=engine,index=False)
